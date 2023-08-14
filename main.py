7d6eee354e80f6f1b8a9c68feb10eb7c2d44b841
Владимир Сорокин <76881066+bohems@users.noreply.github.com>
2023-08-14 17:18:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def is_package_installed(package_name):
         return False
 
 def install_package(package_name):      
-    if not is_package_installed(package_name):
-        print(f"Устанавливаю пакет {package_name}...")
+    if not is_package_installed(package_name):        
         subprocess.check_call(["pip", "install", package_name])
+        print(f"Пакет {package_name} успешно установлен")
     else:
-        print(f"Пакет {package_name} уже установлен.")
+        print(f"Пакет {package_name} уже был ранее установлен.")
