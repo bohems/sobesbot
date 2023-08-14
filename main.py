fecfa3a9b85830f1d83f53809a8b2b3bb227c6f8
Владимир Сорокин <76881066+bohems@users.noreply.github.com>
2023-08-14 16:49:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-def testfunc():
-  return 1
+from google.colab import auth
+from google.auth import default
+
+import gspread
+
+!pip install gspread_formatting
+from gspread_formatting import *
+
+def authorize_in_google_services():
+  auth.authenticate_user()
+  creds, _ = default()
